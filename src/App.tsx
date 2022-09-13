01e564a5564043c2be2a2f1cb4a772f274d1596c
sarthak kumar <sarthakkumar10151@gmail.com>
2022-09-13 23:39:03 +0530

    Replaced css files with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	text-align: center;
+	width: 100vw;
+	height: 100vh;
+
+	display: flex;
+	flex-direction: column;
+`;
+
+const Header = styled.div`
+	border: 1px solid blue;
+	padding: 15px 0;
+`;
 
 function App() {
 	return (
-		<div className="App">
-			<div>Graph Project ...</div>
-		</div>
+		<Container>
+			<Header>Graph Project ...</Header>
+		</Container>
 	);
 }
 
